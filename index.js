4cca7578644dde439874645170a7d7e3eb0fbfe4
ruthwikreddy09 <druthwik09@gmail.com>
2023-09-17 23:21:45 +0530

    added host

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Tesseract = require('tesseract.js');
 const express = require('express');
 var cors = require("cors");
 const app = express();            
-const port = 5000;   
+const port = process.env.PORT || 5000;   
 app.use(cors()); 
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb',extended: true }));
@@ -25,6 +25,6 @@ app.post('/convert', (req, res) => {
     
 }); 
 
-app.listen(port, () => {      
+app.listen(port,"0.0.0.0", () => {      
     console.log(`Now listening on port ${port}`); 
 });
