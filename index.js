d7c132f6b7e1fb9184d1a1cd1c1c6afd9e4b5576
ruthwikreddy09 <druthwik09@gmail.com>
2023-09-17 21:43:08 +0530

    first release

---
+++ index.js
@@ -0,0 +1,26 @@
+const Tesseract = require('tesseract.js');
+const express = require('express');
+var cors = require("cors");
+const app = express();            
+const port = 5000;   
+app.use(cors()); 
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb',extended: true }));
+
+app.post('/convert', (req, res) => {
+    Tesseract.recognize(
+        `${req.body.text}`,
+        'eng',
+        // { logger: m => console.log(m) }
+      ).then(({ data: { text } }) => {
+        console.log(text);
+        res.send({output:text});     
+      }).catch((err)=>{
+        console.warn(err)
+      })
+    
+}); 
+
+app.listen(port, () => {      
+    console.log(`Now listening on port ${port}`); 
+});
